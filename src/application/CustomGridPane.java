5a393e38a5c357adbc2a8bd281c3201aa64c1ff1
kmitchell8 <kevon_11@hotmail.com>
2025-11-06 00:10:09 -0500

    lab6-2

--- src/application/CustomGridPane.java
+++ src/application/CustomGridPane.java
@@ -23,7 +23,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+//import java.sql.Statement;
 import java.sql.PreparedStatement;
 
 public class CustomGridPane /* extends GridPane */ {
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module Lab5_Fx_Wk8 {
 	requires javafx.controls;
 	requires javafx.graphics;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
